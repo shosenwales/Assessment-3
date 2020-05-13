704457a513709899862bfdecbfa508078a585ad2
Shuaib Oseni <wales149@gmail.com>
2020-05-13 14:31:53 +0100

    Update task.sql

--- task.sql
+++ task.sql
@@ -5,5 +5,5 @@ SELECT * FROM Users WHERE Status LIKE '%Pending';
 SELECT * FROM Users WHERE Age > 18 and Age < 26;
 SELECT * FROM Users WHERE Created_dt LIKE '02-01-2020';
 SELECT * FROM Users WHERE Created_dt BETWEEN 05-01-2020 AND 06-01-2020;
-
+UPDATE * Users SET Status = 'Activated';
 UPDATE Users SET name = 'Timothy' WHERE id = 2;
